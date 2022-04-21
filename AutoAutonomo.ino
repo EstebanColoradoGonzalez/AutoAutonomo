7a335236d9aeb189286b440a2a432990fb9fbd8b
Klein Suarez <yahir.kleins@gmail.com>
2022-04-21 18:29:24 -0500

    versión 1 del controlador (Ultrasonido)

--- AutoAutonomo.ino
+++ AutoAutonomo.ino
@@ -1,117 +1,148 @@
-// Modulo L298N (Puente H) --> Control motores DC
-// Motor A
-int ENA = 3;
-int IN1 = 2;
-int IN2 = 4;
+// Importaciónde de Librerias
+#include <Average.h>
 
-// Motor B
-int ENB = 5;
-int IN4 = 6;
-int IN3 = 7;
 
-//Ultrasonido
-int TRIGGER = 8;
-int ECHO = 9;
+/*
+   Modulo L298N (Puente H) --> Control motores DC
+   cosntantes de las conexiones de cada uno de los pines L298N/Arduino
+
+*/
+#define ENA  3
+#define IN1  2
+#define IN2  4
+
+#define ENB  5
+#define IN4  6
+#define IN3  7
+
+/*
+   Sensor Ultrasonido analogico SH-05
+
+*/
+#define TRIGGER 8
+#define ECHO  9
 
 float DURACION;
 float distancia;
-//int velocidadA = 70;
-//int velocidadB = 100;
 
-//Mathematical model
+
+/*
+   Modelo de control
+   definimos el valor deseado, así como las las contantes para cada unos de los motores.
+   variables para calculos
+
+*/
 float valorDeseado = 20;
-//float k1 = 20 / 3;
-//float k2 = 8;
-float k1 = 20 / 6;
-float k2 = 6;
+#define kl 4
+#define kr 7
+float mDistancia;
 float error;
-//float distance[5];
+Average<float> distancias(5);
+
+/*
+   LED azul indicador de proceso retroceder
+
+*/
+#define LED_RETROCESO 13
 
 void setup() {
+  // pines motores
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  // pines ultrasonido
   pinMode(TRIGGER, OUTPUT);
   pinMode(ECHO, INPUT);
   digitalWrite(TRIGGER, LOW);
+
+  // pin LED
+  pinMode(LED_RETROCESO, OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  //llenarVector();
-  //vaciarVector();
+  mDistancia = calcularModa();
+  //int MDistancia = distancias.mean();
+  //Serial.println("-------------");
+  //Serial.println((String) "Moda: " + mDistancia);
+  //Serial.println((String) "Media: " + MDistancia);
+  error = calcularError(mDistancia);
+  //Serial.println((String) "error: "+error);
+  //girarLlantaDerechaHaciaAdelante(calcularVelocidadM2(error));
+  //Serial.println("-------------------");
   //Serial.println((String) "Distancia: " + medirDistancia());
-    error = calcularError();
-    //Serial.println((String) "Error: " + error);
-    //Serial.println((String) "velocidad: " + calcularVelocidadM1(error));
-    //Serial.println((String) "velocidad2: " + calcularVelocidadM2(error));
-    if (error > 0) {
-      retroceder(calcularVelocidadM1(error), calcularVelocidadM2);
-      //Serial.println("Retrocede");
-    } else if (error < 0) {
-      avanzar(calcularVelocidadM1(error), calcularVelocidadM2(error));
-      //Serial.println("Avanza");
-    } else {
-      pararMotorA();
-      pararMotorB();
-    }
- //delay(2000);
-}
-
-float calcularError() {
-  return valorDeseado - medirDistancia();
-}
-
-int calcularVelocidadM1(float error) {
-  if(abs(error) * k1 > 255){
-    return 255;
-  }else{
-    return (int) abs(error) * k1;
+  if (error > 0) {
+    retroceder(calcularVelocidadM2(error), calcularVelocidadM2(error));
+    digitalWrite(LED_RETROCESO, HIGH);
+  } else if (error < 0) {
+    avanzar(calcularVelocidadM1(error), calcularVelocidadM1(error));
+    digitalWrite(LED_RETROCESO, LOW);
+  } else {
+    pararMotorA();
+    pararMotorB();
+    digitalWrite(LED_RETROCESO, LOW);
   }
-  
-}
-
-int calcularVelocidadM2(float error) {
-   if(abs(error) * k2 > 233){
-    return 233;
-  }else{
-   return (int) abs(error) * k2;
-  }
+  //delay(1000);
 }
 
 /*
-  void llenarVector() {
-  for (int index = 0; index < 5; index++) {
-    distance[index] = medirDistancia();
-    //distance[index] = 5;
-    Serial.println(distance[index]);
-  }
-  }
+   Métodos matemáticos para el control
 
-  void vaciarVector(){
-  for (int index = 0; index < 5; index++) {
-    distance[index] = 0;
-    Serial.println(distance[index]);
-  }
-  }
 */
+int calcularModa() {
+  llenarDistancias();
+  //delay(1000);
+  return distancias.mode();
 
-float medirDistancia() {
-  digitalWrite(TRIGGER, HIGH);
-  delayMicroseconds(10);          //Enviamos un pulso durante 10us
-  digitalWrite(TRIGGER, LOW);
+}
 
-  DURACION = pulseIn(ECHO, HIGH);
-  if(DURACION / 59 < 0){
-    medirDistancia();
+float calcularError(int rmDistancia) {
+  return valorDeseado - rmDistancia;
+}
+
+int calcularVelocidadM1(float error) {
+  if (abs(error) * kl > 255) {
+    return 255;
+  } else {
+    return (int) abs(error) * kl;
   }
+
+}
+
+int calcularVelocidadM2(float error) {
+  if (abs(error) * kr > 255) {
+    return 255;
+  } else {
+    return (int) abs(error) * kr;
+  }
+}
+
+void llenarDistancias() {
+  for (int index = 0; index < 5; index++) {
+    int d = medirDistancia();
+    distancias.push(d);
+    //Serial.println((String) "Distancia: " + d);
+    //Serial.println(distance[index]);
+  }
+}
+
+int medirDistancia() {
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGER, LOW);
+  DURACION = pulseIn(ECHO, HIGH);
   return DURACION / 59;
 }
 
+/*
+   Métodos para el control de los motoreductores DC
+
+*/
 void pararMotorA() {
   analogWrite(ENA, 0);
 }
@@ -120,14 +151,16 @@ void pararMotorB() {
   analogWrite(ENB, 0);
 }
 
-void retroceder(int velocidadA, int velocidadB) {
+void avanzar(int velocidadA, int velocidadB) {
   girarLlantaIzquierdaHaciaAdelante(velocidadA);
   girarLlantaDerechaHaciaAdelante(velocidadB);
+  Serial.println("Avanza");
 }
 
-void avanzar(int velocidadA, int velocidadB) {
+void retroceder(int velocidadA, int velocidadB) {
   girarLlantaIzquierdaHaciaAtras(velocidadA);
   girarLlantaDerechaHaciaAtras(velocidadB);
+  Serial.println("Retrocede");
 }
 
 void Izquierda(int velocidadA, int velocidadB) {
